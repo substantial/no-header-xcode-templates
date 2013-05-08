fac8def24dc6cbe943367b7326c753c8db75142f
Aja Hammerly <aja@substantial.com>
2013-05-08 15:50:47 -0700

    No header versions of all templates that ship with Xcode

---
+++ Cocoa (NO-HEADER)/Objective-C class (NO-HEADER).xctemplate/NSView/___FILEBASENAME___.m
@@ -0,0 +1,20 @@
+#import "___FILEBASENAME___.h"
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    // Drawing code here.
+}
+
+@end
---
+++ Cocoa (NO-HEADER)/Objective-C class (NO-HEADER).xctemplate/NSViewController/___FILEBASENAME___.m
@@ -0,0 +1,19 @@
+#import "___FILEBASENAME___.h"
+
+@interface ___FILEBASENAMEASIDENTIFIER___ ()
+
+@end
+
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
